f7487469e9d82f161f28b260c2feb887844d09b0
yulian-art <1119253058@qq.com>
2026-08-13 21:11:13 +0800

    666

--- 知识储备/gin07/main.go
+++ 知识储备/gin07/main.go
@@ -1,2 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("hello failed")
+		return
+	}
+	t.Execute(w, "111")
+
+}
+
+func world(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./world.tmpl")
+	if err != nil {
+		fmt.Println("world failed")
+		return
+	}
+	t.Execute(w, "11122")
+
+}
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/world", world)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println("failed")
+		return
+	}
+}
