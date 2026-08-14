f6337ddc03c00552ab17a1d8e63236bea2f5114e
yulian-art <1119253058@qq.com>
2026-08-14 21:20:50 +0800

    222

--- 知识储备/gin19/main.go
+++ 知识储备/gin19/main.go
@@ -1,37 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type UserInfo struct {
-	ID     int
-	Name   string
-	Gender string
-	Hobby  string
+type User struct {
+	gorm.Model
+	Name         string
+	Age          sql.NullInt64
+	Birthday     *time.Time
+	Email        string `gorm:"type:varchar(120);unique_index"`
+	Role         string `gorm:"size:255"`
+	MemberNumber string `gorm:"unique;nut null"`
+	Num          int    `gorm:"AUTO_INCREMENT"`
+	Address      string `gorm:"index:addr"`
+	IgnoreMe     int    `gorm:"-"`
+}
+type Animals struct{
+	AniID int `gorm:"primaty_key"`
+	Age int
+	Type string
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "root:LYX88888888@(127.0.0.1:3306)/db1?charset=utf8mb4")
+	db, err := gorm.Open("mysql", "root:LYX88888888@(127.0.0.1:3306)/db1")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	db.AutoMigrate(&UserInfo{})
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Animals{})
 
-	// u1 := UserInfo{
-	// 	1, "julien", "woman", "eating",
-	// }
-	// db.Create(&u1)
-	var u UserInfo
-	db.First(&u)
-	fmt.Printf("u:%#v\n", u)
-
-	db.Model(&u).Update("hobby", "sleeping")
-	fmt.Printf("u:%#v\n", u)
-	db.Delete(&u)
 }
