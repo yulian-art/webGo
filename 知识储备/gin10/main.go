d8b9040355a3cea617653413e13ce939b9ecf6a5
yulian-art <1119253058@qq.com>
2026-08-14 12:56:31 +0800

    888

--- 知识储备/gin10/main.go
+++ 知识储备/gin10/main.go
@@ -1,9 +1,43 @@
-package gin10
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r:= gin.Default()
+	r := gin.Default()
 
-	r.GET("/json", func(c *gin.))
+	r.GET("/json", func(c *gin.Context) {
+		// data := map[string]any{
+		// 	"name" : " julien ",
+		// 	"age" : "19",
+		// 	"hobby" : "eating",
+		// }
+
+		data := gin.H{
+			"name":  " julien ",
+			"age":   "19",
+			"hobby": "eating",
+		}
+		c.JSON(http.StatusOK, data)
+	})
+
+	type msg struct {
+		Name  string
+		Age   int
+		Hobby string
+	}
+
+	r.GET("/struct", func(c *gin.Context) {
+		data := msg{
+			Name:  "julien",
+			Age:   18,
+			Hobby: "sing",
+		}
+		c.JSON(http.StatusOK, data)
+	})
+
+	r.Run(":9000")
 }
