2459e90f99f29160d98927afdee153d03eb8efd6
yulian-art <1119253058@qq.com>
2026-08-14 22:20:12 +0800

    222多行查询

--- smallList/go.mod
+++ smallList/go.mod
@@ -2,7 +2,10 @@ module main.go
 
 go 1.26.3
 
-require github.com/gin-gonic/gin v1.12.0
+require (
+	github.com/gin-gonic/gin v1.12.0
+	github.com/jinzhu/gorm v1.9.16
+)
 
 require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
@@ -14,8 +17,10 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.30.1 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
---
+++ 知识储备数据库/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func initMySQL()(err error){
+	dsn := "root:LYX88888888@tcp(127.0.0.1:3306)/db1"
+	db, err = sql.Open("mysql", dsn)
+	if err!= nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("connect failed %v \n", err)
+		return
+	}
+
+	db.SetConnMaxLifetime(time.Second * 10)
+	db.SetConnMaxIdleTime(200)
+	db.SetMaxOpenConns(10)
+
+	return
+}
+
+type User struct{
+	id int
+	age int
+	name string
+}
+
+func queryRowDemo(){
+	sqlStr := "select id, name, age from user where id=?"
+
+	var u User
+
+	row:= db.QueryRow(sqlStr, 1)
+	
+	err := row.Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("Scan failed %v \n",err)
+		return
+	}
+	fmt.Printf("id : %d name %s age : %d", u.id, u.name, u.age)
+
+}
+
+func main(){
+	if err := initMySQL(); err != nil{
+		fmt.Printf("connect to db failed , err : %v \n", err)
+	}
+	defer db.Close()
+	queryRowDemo()
+
+}
