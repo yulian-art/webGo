597f115fce7295d563aec3f100886ed63946db77
yulian-art <1119253058@qq.com>
2026-08-14 09:59:28 +0800

    777增加自定义解析函数

--- 知识储备/gin09/main.go
+++ 知识储备/gin09/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(str string) template.HTML{
+			return template.HTML(str)
+		},
+	})
 	r.LoadHTMLGlob("templates/**/*")
 	r.GET("posts/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
@@ -16,7 +22,7 @@ func main() {
 	})
 	r.GET("users/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
-			"title": "julien",
+			"title": "<a herf='https://julien.net.cn'>julien s blog</a>",
 		})
 	})
 	r.Run(":9000")
